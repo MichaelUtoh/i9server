6ab31532e07e61285873d7cb2509b38057197c15
michael <michaelutoh21@gmail.com>
2022-10-27 14:08:56 +0100

    Simple server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  data = {
+        "slackUsername": "Michael Utoh",
+        "backend": true,
+        "age": 30,
+        "bio": "I am a backend developer based in Lagos, Nigeria. I write efficient and scalable APIs using Python (Django and Rest Framework)"
+    }
+
+  res.send(data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
